c8e7cbd02f41ddfe7cb97af0e538b60c3682a20e
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-08-03 00:03:54 +0100

    Initial commit

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,54 @@
+- What does the product solve?
+- How does the product solve it?
+
+
+
+
+
+- What are customers pain points?
+
+
+Sales Prep Pain Points
+
+
+
+function LandingHome() {
+  return (
+    <div className="flex h-full">
+
+      <header>
+
+      </header>
+
+      <section id="hero">
+
+      </section>
+
+      <section id="logos">
+
+      </section>
+
+      <section id="benefits">
+      What does it solve?
+      Automating Research and Presentation
+
+      Geneats Slides and Documents
+      </section>
+
+      <section id="how-it-works">
+
+      </section>
+
+      <section id="cta">
+
+      </section>
+
+      <footer>
+
+      </footer>
+
+    </div>
+  );
+}
+
+export default LandingHome
